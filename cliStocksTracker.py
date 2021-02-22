a09c07931561c00853c869e0a6e2c72962f8aaa0
James Dixon <notjamesdixon@gmail.com>
2021-02-21 22:12:14 -0500

    move auto_colors to a module and remove matplotlib for now

--- cliStocksTracker.py
+++ cliStocksTracker.py
@@ -9,8 +9,8 @@ import numpy as np
 import yfinance as market
 
 import utils
+import autocolors
 
-from matplotlib import colors
 from colorama import Fore, Style
 from datetime import datetime, timedelta
 
@@ -22,15 +22,6 @@ def main():
     portfolio = Portfolio()
     graphs = []
 
-    auto_colors = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#00FFFF", "#000000", 
-        "#800000", "#008000", "#000080", "#808000", "#800080", "#008080", "#808080", 
-        "#C00000", "#00C000", "#0000C0", "#C0C000", "#C000C0", "#00C0C0", "#C0C0C0", 
-        "#400000", "#004000", "#000040", "#404000", "#400040", "#004040", "#404040", 
-        "#200000", "#002000", "#000020", "#202000", "#200020", "#002020", "#202020", 
-        "#600000", "#006000", "#000060", "#606000", "#600060", "#006060", "#606060", 
-        "#A00000", "#00A000", "#0000A0", "#A0A000", "#A000A0", "#00A0A0", "#A0A0A0", 
-        "#E00000", "#00E000", "#0000E0", "#E0E000", "#E000E0", "#00E0E0", "#E0E0E0"]
-    
     for stock in stocks_config:
         new_stock = Stock(stock)
 
@@ -71,11 +62,11 @@ def main():
         for stock in portfolio.get_stocks():
             if stock.graph:
                 graphing_list.append(stock)
-        graphs.append(Graph(graphing_list, int(config["frame"]["width"]), int(config["frame"]["height"]), auto_colors[:len(graphing_list)]))
+        graphs.append(Graph(graphing_list, int(config["frame"]["width"]), int(config["frame"]["height"]), autocolors.color_list[:len(graphing_list)]))
     else:
         for i, stock in enumerate(portfolio.get_stocks()):
             if stock.graph:
-                graphs.append(Graph([stock], int(config["frame"]["width"]), int(config["frame"]["height"]), [auto_colors[i]]))
+                graphs.append(Graph([stock], int(config["frame"]["width"]), int(config["frame"]["height"]), [autocolors.color_list[i]]))
 
     # generate and print the graphs
     for graph in graphs:
